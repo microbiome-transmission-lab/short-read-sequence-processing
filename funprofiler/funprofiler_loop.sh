c41d9dd2c70ad2100172e537ac20b7afcc38bcae
Brendan J. Kelly <brendank@upenn.edu>
2024-02-27 11:00:06 -0500

    Cleanup demo. Add funprofiler bash script.

---
+++ funprofiler/funprofiler_loop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if a directory is provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide a directory path"
+  exit 1
+fi
+
+# Store the provided directory path
+input_dir=$1
+output_dir=$2
+
+# Specify the name of your Python script
+python_script="$HOME/funprofiler/funcprofiler.py"  # Replace with the actual name of your script
+
+# Note: Zenodo source for FracMiniHash sketches of Orthologs in KEGG database from the KoslickiLab: https://zenodo.org/records/10045253
+koslicki_ko_sketch="$HOME/funprofiler/demo/KOs_sketched_scaled_1000.sig.zip"
+
+# Loop through all files in the directory
+for file in "$input_dir"/*; do
+  if [ -f "$file" ]; then  # Make sure it's a regular file
+    filename=$(basename "$file")
+    output_file_ko="${filename%.*}_funprofiler_ko_profiles.csv"  # Example output naming
+    output_file_gather="${filename%.*}_funprofiler_gather_out.csv" # Second output name
+
+    # Run the Python script, passing input and output filenames
+    python "$python_script" "$file" "$koslicki_ko_sketch" 7 1000 "$output_dir/$output_file_ko" -g "$output_dir/$output_file_gather"
+  fi
+done
+
+
